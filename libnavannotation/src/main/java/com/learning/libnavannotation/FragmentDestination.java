e5058c0acdde62b21e4214749ff79175e3d74e43
Richen.Xu <490313997@qq.com>
2020-11-07 00:35:58 +0800

    自定义注解&注解解析器

---
+++ libnavannotation/src/main/java/com/learning/libnavannotation/FragmentDestination.java
@@ -0,0 +1,13 @@
+package com.learning.libnavannotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface FragmentDestination {
+    String pageUrl();
+
+    boolean needLogin() default false;
+
+    boolean asStart() default false;
+}
